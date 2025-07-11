b640718649ccdd058d69e90fc90a93ea62c3c929
anithrajakumar <79019132+anithrajakumar@users.noreply.github.com>
2025-07-11 17:49:59 -0400

    Add files via upload

---
+++ Lab1/simulation/qsim/work/lab1_vhdl2/_primary.vhd
@@ -0,0 +1,12 @@
+library verilog;
+use verilog.vl_types.all;
+entity lab1_vhdl2 is
+    port(
+        w1              : in     vl_logic;
+        w2              : in     vl_logic;
+        w3              : in     vl_logic;
+        w4              : in     vl_logic;
+        g               : out    vl_logic;
+        h               : out    vl_logic
+    );
+end lab1_vhdl2;
